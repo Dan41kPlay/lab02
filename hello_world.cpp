4db618726340e975155a02f47dca19a196e81276
dp229 <daniel.zakirov.06@gmail.com>
2026-02-27 16:30:48 +0300

    updated comments in hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-    std::string name; // name of the user will be stored here
+    std::string name; // name of the user will be stored here (umm aktually this is a different comment now get rekt)
     std::cout << "Enter your name: "; // prompt the user to enter their name
     std::cin >> name; // read the name
     std::cout << "Hello world from " << name << std::endl; // print greetings to the world from the user
